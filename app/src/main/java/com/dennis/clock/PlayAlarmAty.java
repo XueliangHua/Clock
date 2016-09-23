3603e0c8ac278dba0adcbd2606b4d34b4a48cd4f
XueliangHua <xueliang2832@gmail.com>
2016-09-23 09:25:19 +0800

    Initial commit

---
+++ app/src/main/java/com/dennis/clock/PlayAlarmAty.java
@@ -0,0 +1,36 @@
+package com.dennis.clock;
+
+import android.app.Activity;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+
+/**
+ * Created by Xueliang Hua on 2016/9/22.
+ */
+
+public class PlayAlarmAty extends Activity {
+    private MediaPlayer mp;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.alarm_player_aty);
+
+        mp = MediaPlayer.create(this, R.raw.music);
+        mp.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        finish();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        mp.stop();
+        mp.release();
+    }
+}
